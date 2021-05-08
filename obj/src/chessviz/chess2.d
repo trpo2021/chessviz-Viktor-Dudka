a88db160fec778fa9ace28127b53c4145933415d
Viktor-Dudka <petruchok02@mail.ru>
2021-05-08 16:37:48 +0700

    Delete *.d and *.a files

--- obj/src/chessviz/chess2.d
+++
@@ -1,4 +0,0 @@
-obj/src/chessviz/chess2.o: src/chessviz/chess2.cpp \
- C:\Users\petru\source\repos\chessviz-Viktor-Dudka\src\libchessviz\lib.h
-
-C:\Users\petru\source\repos\chessviz-Viktor-Dudka\src\libchessviz\lib.h:
--- obj/src/libchessviz/lib.d
+++
@@ -1,4 +0,0 @@
-obj/src/libchessviz/lib.o: src/libchessviz/lib.cpp \
- C:\Users\petru\source\repos\chessviz-Viktor-Dudka\src\libchessviz\lib.h
-
-C:\Users\petru\source\repos\chessviz-Viktor-Dudka\src\libchessviz\lib.h:
